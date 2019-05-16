b5cacda02126780a8a26f013a35b9c565ffcf0ec
root <root@srv-test-01.minsk.epam.com>
2019-05-16 08:50:55 +0300

    add script create ant add job

---
+++ add_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -s -XPOST 'http://localhost:8080/createItem?name=seed' -u admin:admin  --data-binary @config.xml -H "Content-Type:text/xml" > /dev/null 
+if [ $? -eq 0 ] ;  
+  then echo 'job add in jenkins and create pipeline'
+  curl -X POST http://admin:admin@localhost:8080/job/seed/build
+else 
+   echo 'jon not add jenkins' 
+fi
