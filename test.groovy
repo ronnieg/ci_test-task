bfc7556446e16c50240b22c7ab77be5233656e85
AndreyNovik1993 <43274094+AndreyNovik1993@users.noreply.github.com>
2019-05-21 09:31:28 +0300

    Create test.groovy

---
+++ test.groovy
@@ -0,0 +1,20 @@
+folder('job_dsl') { 
+
+   description('folder for pipeline job')
+  
+}
+pipelineJob('job_dsl/pipeline') {
+      def repo = 'https://github.com/AndreyNovik1993/ci_test-task.git' 
+      description("Pipeline for $repo") 
+      definition { 
+          cpsScm { 
+             scm { 
+               git { remote { url(repo) } 
+               branches('master', '**/feature*') 
+               scriptPath('Jenkinsfile') 
+               extensions { } 
+		}
+	 } 
+	} 
+ } 
+}
