cf0074c59fc84f16d5b1da59687de34905a40460
AndreyNovik1993 <43274094+AndreyNovik1993@users.noreply.github.com>
2019-05-21 11:59:03 +0300

    Update add_job.sh

--- add_job.sh
+++ add_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl -s -XPOST 'http://localhost:8080/createItem?name=seed' -u admin:admin  --data-binary @config.xml -H "Content-Type:text/xml" > /dev/null 
 if [ $? -eq 0 ] ;  
-  then echo 'job add in jenkins and create pipeline'
-  curl -X POST http://admin:admin@localhost:8080/job/seed/build
+  then echo 'job add in jenkins and create pipeline. Please wait 30s'
+  sleep 30 && curl -X POST http://admin:admin@localhost:8080/job/seed/build
   if [ $? -eq 0 ]
     then echo 'job seeb will work  after 4s' 
   else
